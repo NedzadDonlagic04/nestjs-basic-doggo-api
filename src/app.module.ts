5af3a584fdae91e73ac7c42d3ec2eb39aae97ad5
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-12 13:50:09 +0200

    Imported needed controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DoggoController } from './doggo/doggo.controller';
+import { DoggoService } from './doggo/doggo.service';
 
 @Module({
   imports: [],
   controllers: [DoggoController],
-  providers: [],
+  providers: [DoggoService],
 })
 export class AppModule {}
